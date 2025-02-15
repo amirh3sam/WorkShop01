19b678c6d5ac6055eb33bbe664e83575f9aeb6ab
amirhesam <amirh3sam.badiei@gmail.com>
2025-02-15 11:19:12 -0500

    update

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue, push } from "firebase/database"; // Add push here
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzmV3MTGzopLGgehC5LP4oSbpuTr_VfyQ",
+  authDomain: "workshop-38d39.firebaseapp.com",
+  databaseURL: "https://workshop-38d39-default-rtdb.firebaseio.com",
+  projectId: "workshop-38d39",
+  storageBucket: "workshop-38d39.appspot.com",
+  messagingSenderId: "914740664427",
+  appId: "1:914740664427:web:cfd41d2ccc21b3e329f92e",
+  measurementId: "G-0KML5V31EQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database, ref, set, onValue, push }; // Export push here
